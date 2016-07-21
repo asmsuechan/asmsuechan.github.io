22df7e3673995ef2f2f85f0178d70df283be70ab
asmsuechan <suenagaryoutaabc@gmail.com>
2016-07-22 01:01:04 +0900

    Fixes appjs

--- gcm_service_worker/app.js
+++ gcm_service_worker/app.js
@@ -1,12 +1,20 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', e => {
-
-  navigator.serviceWorker.register('service-worker.js').then(registration => {
-
-    registration.pushManager.subscribe().then(subscription => {
-      console.log('GCM engpoint is: ', subscription.endpoint);
-    });
-
-  }).catch(error => console.log(error));
-});
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(function(registration) {
+        return registration.pushManager.getSubscription().then(function(subscription) {
+          if (subscription) {
+            return subscription
+          }
+          return registration.pushManager.subscribe({
+            userVisibleOnly: true
+          })
+        })
+      }).then(function(subscription) {
+        var endpoint = subscription.endpoint
+        console.log("pushManager endpoint:", endpoint) // https://android.googleapis.com/gcm/send/******:******......
+      }).catch(function(error) {
+        console.warn("serviceWorker error:", error)
+      })
+  }
+})
